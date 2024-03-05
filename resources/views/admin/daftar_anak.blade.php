a74d2e9f9df0c674dc4c55bd4bcdab5922941c1e
RafhyAriiq <rafhyariiq5@gmail.com>
2024-03-05 11:56:40 +0700

    Update daftar_anak.blade.php

--- resources/views/admin/daftar_anak.blade.php
+++ resources/views/admin/daftar_anak.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="card">
+    <div class="card-header">
+    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <a href="#" class="btn btn-success btn-icon-split">
+                <span class="icon text-white-50">
+                    <i class="fas fa-plus"></i>
+                </span>
+                <span class="text">Tambah Anak</span>
+            </a>
+            <div class="mr-3"></div>
+            <a href="#" class="btn btn-info btn-icon-split">
+                <span class="icon text-white-50">
+                    <i class="fas fa-search"></i>
+                </span>
+                <span class="text">Cari Anak By NIK</span>
+            </a>
+        </div>
+    </div>
+</div>
 @endsection
