b001eb7bb350dadc0fa961b9d9e5540a79db6d25
RafhyAriiq <rafhyariiq5@gmail.com>
2024-03-05 12:59:36 +0700

    Update daftar_anak.blade.php

--- resources/views/admin/daftar_anak.blade.php
+++ resources/views/admin/daftar_anak.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="card">
     <div class="card-header">
-    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+    <h6 class="m-0 font-weight-bold text-primary">Daftar Balita</h6>
     </div>
     <div class="card-body">
         <div class="row">
@@ -21,6 +21,54 @@
                 <span class="text">Cari Anak By NIK</span>
             </a>
         </div>
+
+            <div class="mb-3"></div>
+            <div class="table-responsive">
+                <table class="table table-bordered"id="tableAnak" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>NIK</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Tgl Lahir</th>
+                            <th>Nama Ortu</th>
+                            <th>No Telp Ortu</th>
+                            <th>Tindakan </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>9876546</td>
+                            <td>ASEP</td>
+                            <td>Lekong</td>
+                            <td>7 Masehi</td>
+                            <td>Bagas Dribble</td>
+                            <td>09678987</td>
+                            <td><div class="row justify-content-center">
+                            <a href="#" class="btn btn-success btn-circle btn-sm">
+                                        <i class="fas fa-plus"></i>
+                                    </a>
+                                    <div class="mx-1"></div>
+                            <a href="#" class="btn btn-danger btn-circle btn-sm">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </a>
+                                    <div class="mx-1"></div>
+                                    <a href="#" class="btn btn-info btn-circle btn-sm">
+                                        <i class="fas fa-search"></i>
+                                    </a>
+                                    <div class="mx-1"></div>
+                                    <a href="#" class="btn btn-secondary btn-circle btn-sm">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <div class="mx-1"></div>
+                            </div></td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
     </div>
 </div>
 @endsection
