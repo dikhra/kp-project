6b89043f979da89e54861385a78789025c5afd23
RafhyAriiq <rafhyariiq5@gmail.com>
2024-03-13 16:27:58 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="page-header">
+    <h1>SISTEM INFORMASI STUNTING</h1>
+</div>
     <div class="row">
         <div class="card">
             <div class="card-header">
-                Contoh
+                Data Anak Masuk
             </div>
             <div class="card-body">
-                contoh body
+                3
             </div>
         </div>
     </div>
